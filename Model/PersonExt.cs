c3eabce20523ee6b3ae5805826378fca6b55c76a
kevinbowe <kevinbowe1957@gmail.com>
2024-07-27 14:26:28 -0400

    WIP - Merge PersonExtLandscape.cs & PersonExtPortrait.cs into PersonExt.cs. - Factor changes. --------------------------------------------------    Version:   CL_UI_Scaffold_V1 --------------------------------------------------
       Change Summary
       --------------
    
    Work in Progress.
    
    PersonExtLandscape.cs
    PersonExtPortrait.cs
    --------------------
    Consolidated these methods into PersonExt.cs or factored them, then discarded the files.
    -- OutputPersonFriendlyNamePortrait
    -- PadLeft
    -- OutputPropertiesListPortrait
    -- OutputPersonFriendlyName
    -- OutputPropertiesList
    
    PersonExt.cs
    ------------
    Modified StringBuilder PadLeft(~) to return a StringBuilder object/
    Add string PadLeft(~) to
    Modified OutputProperties(~)
    Add PadFriendlyNameLandscape(~)
    -- This a factoring of OutputPersonFriendlyName(~)
    Generally factor out duplicate code and simplify execution flow.
    
    --------------------------------------------------
       modified:   Model/   PersonExt.cs
       deleted:   Model/   PersonExtLandscape.cs
       deleted:   Model/   PersonExtPortrait.cs
    --------------------------------------------------

--- Model/PersonExt.cs
+++ Model/PersonExt.cs
@@ -1,19 +1,50 @@
 namespace CL_UI_Scaffold_V1;
 using System.Reflection;
 using System.Text;
+using static Helper;
 
 public static class PersonExt
 {
-	public static void PadLeft(this StringBuilder strBuilder, int spaces)
-	{
-		strBuilder.Insert(0," ", spaces);	
-	}
+	public static StringBuilder SbPadLeft (StringBuilder strBuilder, int spaces) => strBuilder.Insert(0," ", spaces);
 
-	// public static string PadLeftLineFeed(this string str, int spaces)
-	// {
-	// 	for(int i = 1; i <= spaces; i++) str = ' ' + str; 
-	// 	return str + "\n";
-	// }
+	public static Func<string , int, string> PadLeft = (str, spaces) => {
+		for(int i = 1; i <= spaces; i++) str = ' ' + str; 
+		return str;
+	};
+
+	public static StringBuilder OutputProperties(
+		this Person person, string[]? personPropertyFilterList = null, string orientation = "landscape") 
+	{
+		var strBuilder = new StringBuilder();
+		
+		//		Get all of the property names in the Person Class.
+		IList<PropertyInfo> personPropInfoList = new List<PropertyInfo>(person.GetType().GetProperties());
+
+		//			Is there a Property FilterList ?
+		if (personPropertyFilterList != null) 
+		{
+			//		Search the personPropInfoList looking for a matching property 
+			//		in the personPropertyFilterList
+			foreach(PropertyInfo personPropInfo in personPropInfoList)
+			{
+				string? propMatch = personPropertyFilterList.Where(e => personPropInfo.Name == e).FirstOrDefault();
+				if (propMatch == null) continue;
+				strBuilder = OutputPersonFriendlyName(person, personPropInfo, strBuilder, orientation);
+			}
+			//		Output the output stringBuilder sting based on the requested orientation: portrait or landscape.
+			return orientation == "landscape" ? SbPadLeft(strBuilder, 5) : strBuilder;
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		//		If we get here; There is a Property FilterList
+
+			// Grab all the properties in the Persons class.
+			foreach(PropertyInfo personPropInfo in personPropInfoList) 
+			{
+				strBuilder = person.OutputPersonFriendlyName(personPropInfo, strBuilder, orientation);
+			}
+		return strBuilder;
+	}
 
 	public static string SetFriendlyName(PropertyInfo personPropInfo)
 	{
@@ -40,45 +71,67 @@ public static class PersonExt
 		return propMatch;
 	}
 
-	public static StringBuilder OutputProperties(
-		this Person person, string[]? personPropertyFilterList = null, string orientation = "landscape") 
+	public static StringBuilder PadFriendlyNameLandscape (StringBuilder strBuilder, string str, PropertyInfo? personPropInfo)
 	{
-		var strBuilder = new StringBuilder();
-
-		//			Is there a Property FilterList ?
-		if (personPropertyFilterList == null) 
+		var padder = 20;
+		//
+		switch(personPropInfo?.Name)
 		{
-			IList<PropertyInfo> personPropInfoList= new List<PropertyInfo>(person.GetType().GetProperties());
-
-				// Grab all the properties in the Persons class.
-				foreach(PropertyInfo personPropInfo in personPropInfoList) 
-				{
-					if (orientation == "landscape")
-					{
-						strBuilder = person.OutputPersonFriendlyNamePortrait(personPropInfo, strBuilder, orientation);
-						// strBuilder = person.OutputPersonFriendlyName(personPropInfo, strBuilder, orientation);
-					}
-					else // orientation == "portrait"
-						strBuilder = person.OutputPersonFriendlyNamePortrait(personPropInfo, strBuilder, orientation);
-				}
-			return strBuilder;
+			case "Id" :
+				strBuilder.Append(str.PadRight(padder,' '));
+				break;
+			case "SSN" :
+				strBuilder.Append(str.PadRight(7+padder,' '));
+				break;
+			case "FirstName" :
+				strBuilder.Append(str.PadRight(13+padder,' '));
+				break;
+			case "LastName" :
+				strBuilder.Append(str.PadRight(16+padder,' '));
+				break;
+			case "CurrentTime_sys" : 	/* Added-On */
+				strBuilder.Append(str.PadRight(28+padder,' '));
+				break;
+			case "InsertDte_sys" : 		/* Added */
+				strBuilder.Append(str.PadRight(20+padder,' '));
+				break;
+			case "WorkStartsAt_sys" : 	/* Start Time */
+				strBuilder.Append(str.PadRight(11+padder,' '));
+				break;
+			case "doB_sys" : 	/* DoB */
+				strBuilder.Append(str.PadRight(7+padder,' '));
+				break;
 		}
-		
-		//////////////////////////////////////////////////////////////////////////
-		//		If we get here; There is a Property FilterList
-
-		if (orientation == "landscape")
-		{
-			strBuilder = person.OutputPropertiesListPortrait(personPropertyFilterList, orientation);
-			// strBuilder = person.OutputPropertiesList(personPropertyFilterList, orientation);
-			//		Indent the completed string.
-			strBuilder.PadLeft(5);	
-			return strBuilder;
-		}
-		else  // Portrait
-		{
-			strBuilder = person.OutputPropertiesListPortrait(personPropertyFilterList, orientation);
-			return strBuilder;
-		}
+		return strBuilder;
 	}
+
+
+	public static StringBuilder OutputPersonFriendlyName( 
+		this Person person, PropertyInfo personPropInfo, StringBuilder strBuilder, string orientation)
+	{
+		var propMatch = SetFriendlyName(personPropInfo);
+
+		//			Fetch the value associated with the actual property name
+		object? propertyValue = personPropInfo?.GetValue(person);
+ 		//		
+ 		if (personPropInfo?.PropertyType.Name == "DateOnly")
+ 		{
+			propertyValue = ObjectToDateOnlyFmt(propertyValue);
+ 		}
+		var str = $"{Menus.GREEN}{propMatch}: {Menus.WHITE} {propertyValue}";
+
+		if (orientation == "portrait")
+		{
+		//			Pad the input string with Nx spaces to the left.
+		//			Append an '\n' to the end of the line.
+		return strBuilder.Append( PadLeft(str,5) + '\n' );
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		//			If we get here, orientation == "landscape".
+
+		return PadFriendlyNameLandscape(strBuilder, str, personPropInfo);
+
+	}
+
 }
